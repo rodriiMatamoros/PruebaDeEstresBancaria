6b9a66b2a268d9643350302b3bb831d263f18247
rodri <rodrigomatamorosbecerra@gmail.com>
2023-10-05 10:14:17 +0200

    commit mostrarMain

--- src/main/java/org/example/Lanzador.java
+++ src/main/java/org/example/Lanzador.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class Lanzador {
-    public static void main(String[] args) {
+    public static void mostrarMain() {
         Cuenta cuenta = new Cuenta(10000);
 
         int numDepositos = 400;
